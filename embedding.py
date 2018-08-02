4af7a696595882ae531583e2872454d11296ffeb
daQuincy <yuquan95@gmail.com>
2018-08-03 00:39:53 +0800

    initial commit

---
+++ embedding.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  2 22:37:23 2018
+
+@author: YQ
+"""
+
+import cv2
+import numpy as np
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--cover_image", required=True,
+                help="path to cover image")
+ap.add_argument("-m", "--message_image", required=True,
+                help="path to message image")
+ap.add_argument("-s", "--stego_image", required=True,
+                help="path to save stego image")
+args = vars(ap.parse_args())
+
+# step 1: read the cover image and message image
+c = args["cover_image"]
+m = args["message_image"]
+
+c_img = cv2.imread(c, 0)
+c_img = cv2.resize(c_img, (256, 256))
+
+m_img = cv2.imread(m, 0)
+m_img = cv2.resize(m_img, (256, 256))
+m_img[m_img>0] = 1
+
+
+# step 2: change the pixel value to binary
+c_flatten = c_img.flatten()
+c_flatten = [np.binary_repr(x, width=8) for x in c_flatten]
+
+m_flatten = np.reshape(m_img, (-1,)).tolist()
+
+
+out = []
+for a, b in zip(c_flatten, m_flatten):
+    # step 3: perform XOR operations on the 7th and on the 6th bit    
+    xor_a = int(a[1]) ^ int(a[2])
+    
+    # step 4: perform XOR operations on 8th bit with xor_a
+    xor_b = int(a[0]) ^ xor_a
+    
+    # step 5: perform XOR operations on message bits with 3 MSB
+    xor_c = int(b) ^ xor_b 
+    
+    # step 6: save xor_c, convert back to uint8
+    save = a[:-1] + str(xor_c)
+    
+    # https://stackoverflow.com/questions/8928240/convert-base-2-binary-number-string-to-int
+    out.append(int(save, 2))
+    
+stego_img = np.array(out)
+stego_img = np.reshape(stego_img, (256, 256))
+
+cv2.imwrite(args["stego_image"], stego_img)
+    
---
+++ extraction.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  3 00:10:15 2018
+
+@author: YQ
+"""
+
+import cv2
+import argparse
+import numpy as np
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--stego_image", required=True,
+                help="path to stego image")
+ap.add_argument("-r", "--recover_image", required=True,
+                help="path to save recovered image")
+args = vars(ap.parse_args())
+
+# step 1: read stego image
+stego_img = cv2.imread(args["stego_image"], 0)
+
+# step 2: change pixel value to binary
+stego_flatten = stego_img.flatten()
+stego_flatten = [np.binary_repr(x, width=8) for x in stego_flatten]
+
+out = []
+for x in stego_flatten:
+    # step 3: perform XOR on 7th and 6th bits
+    xor_a = int(x[1]) ^ int(x[2])
+    
+    # step 4: perform XOR operation on 8th bit with xor_a
+    xor_b = int(x[0]) ^ xor_a
+    
+    # step 5: perform XOR operations on message bits with 3 MSB
+    xor_c = int(x[-1]) ^ xor_b
+    
+    out.append(int(xor_c))
+    
+recover_img = np.reshape(np.array(out), (256,256))
+recover_img[recover_img==1] = 255
+cv2.imwrite(args["recover_image"], recover_img)
